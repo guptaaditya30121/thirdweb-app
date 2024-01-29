a02a5e74965310473fc9f52a37222362a6daad34
aditya <guptaaditya30121@gmail.com>
2024-01-29 15:53:49 +0530

    MAde all functions to interact

--- src/components/Getusercount.js
+++ src/components/Getusercount.js
@@ -1,21 +1,165 @@
 import { Web3Button } from "@thirdweb-dev/react";
-import {useState} from "react";
+
 
 
 export default function Getusercount(){
 
-    const [noOfUser , setNoOfUser] = useState(0);
+    var userAddress = "0x6c47D516004DC29cDb14A5C14D576E41055bDb95";
+    var userAddress2 = "0x14FFe36CB84adEF9C0577C3CB31E596a5C1df403";
+    var contractAddress = "0x59055d567418068Af2ba08a9F36c3505136F8691";
     return (
         <div>
-            <Web3Button contractAddress= "0xAdd87CE1BDAc4d8b0286AcAb9B04eBb25303D560"
+            <Web3Button contractAddress= {contractAddress}
                         action={(contract) => contract.call(
-                            "getAllReqClaims",
-                            [],{from: "0x6c47D516004DC29cDb14A5C14D576E41055bDb95"}
+                            "showUser",
+                            [],{from: userAddress}
                         )}
                         onSuccess={(results)=>{
                             console.log(results);
                         }}
-            > No of user is = ${noOfUser} </Web3Button>
+            > show User </Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "addUser",
+                            [userAddress],{from: userAddress}
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > add User </Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "getNoOfUser",
+                            [],{from: userAddress}
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > get Number Of User </Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "requestClaim",
+                            [userAddress , "Field" , "Value"],{from: userAddress}
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Request Claim </Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "makeClaim",
+                            [userAddress2 , "Field" , "Value"],{from: userAddress}
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Make Claim </Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "getClaimsCount",
+                            [userAddress],{from: userAddress}
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Get Claims Count </Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "getAllClaims",
+                            [],{from: userAddress}
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Get All Claims</Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "getRequestedClaimsCount",
+                            [userAddress],{from: userAddress}
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Get Requested Claims Count </Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "getAllReqClaims",
+                            [],{from: userAddress}
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Get All Request Claims</Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "getReqFromMeCount",
+                            [userAddress],{from: userAddress}
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Get Request From me Count </Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "getAllReqFromMeClaims",
+                            [],{from: userAddress}
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Get All Request From me Claims</Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "answeringRequestedClaim",
+                            [0 , 1],{from: userAddress} //0 is the index of Query , and 1 is the if you want to answer
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Answer Requested Claim </Web3Button>
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "reqFieldInfo",
+                            [userAddress2 , "Field"],{from: userAddress} 
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Request Field Info </Web3Button>            
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "ansRequestFieldInfo",
+                            [0 , 1],{from: userAddress} //index in all the requested field
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Ansser Request Field Info </Web3Button> 
+
+            <Web3Button contractAddress= {contractAddress}
+                        action={(contract) => contract.call(
+                            "getAllinfo",
+                            [],{from: userAddress} //index in all the requested field
+                        )}
+                        onSuccess={(results)=>{
+                            console.log(results);
+                        }}
+            > Get all Info </Web3Button> 
+
+
         </div>
     )
 }
